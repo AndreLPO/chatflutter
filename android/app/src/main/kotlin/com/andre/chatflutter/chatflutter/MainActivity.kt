8cdccc3dfb2710257e1a34333b16e02527d04c6f
André Oliveira <andrelpo573@gmail.com>
2022-03-05 11:03:40 -0300

    Commit inicial do projeto

---
+++ android/app/src/main/kotlin/com/andre/chatflutter/chatflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.andre.chatflutter.chatflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:chatflutter/app_widget.dart';
+import 'package:chatflutter/modules/Home/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() {
+  runApp(ModularApp(module: AppModule(), child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    // Firebase.initializeApp();
+    // FirebaseFirestore.instance.collection('users').add({"texto": "andre"});
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Material(
+              child: Center(
+                child: Text("Erro ao iniciar o Firebase!",
+                    textDirection: TextDirection.ltr),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return const AppWidget();
+          }
+          return const Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        });
+  }
+}
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const HomePage()),
+      ];
+}
